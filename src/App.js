b8575f7edd80cccf53ffe343a0a0485e685a80a8
sepidehsalman <sepidehsalman@gmail.com>
2023-10-04 19:20:04 +0330

    feat: added fetch for get api

--- src/App.js
+++ src/App.js
@@ -4,6 +4,32 @@ import { useState } from "react";
 function App() {
   const [city, setCity] = useState("");
   const [weatherInfo, setWeatherInfo] = useState(null);
+
+  function getWeather() {
+    const apiKey = "";
+    const url = `/api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        // console.log("data", data);
+        //  let MT = Math.round(data.main.temp);
+        let FL = Math.round(data.main.feels_like);
+
+        const weather = {
+          location: `weather in ${data.names}`,
+          tempreture: `tempreture `,
+          feelsLike: `Feels like ${FL}`,
+        };
+
+        setWeatherInfo(weather);
+      })
+      .catch((error) => {
+        // console.error(error);
+      });
+  }
   return (
     <div className="App">
       <input
@@ -12,6 +38,14 @@ function App() {
         value={city}
         onChange={(e) => setCity(e.target.value)}
       />
+      <button onClick={getWeather}>Get Weather</button>
+      {weatherInfo && (
+        <div className="weather-info">
+          <h3>{weatherInfo.FL}</h3>
+          <p>{weatherInfo.location}</p>
+          <p>{weatherInfo.MT}</p>
+        </div>
+      )}
     </div>
   );
 }
